d6f2cc674f12571dd275259128f5da1066e77b7d
Aleksander <zdarovich@gmail.com>
2021-09-16 21:54:41 +0300

    31

--- test/user_registration_test.go
+++ test/user_registration_test.go
@@ -14,7 +14,7 @@ import (
 func Test_Register_User_100_times(t *testing.T) {
 	stats := tachymeter.New(&tachymeter.Config{Size: requestSamples100})
 	testKeyPath := "resources/keys"
-
+	var memoryUsed int
 	var success, fails int
 	for i := 0; i < requestSamples100; i++ {
 		name := uuid.New().String()
@@ -27,6 +27,7 @@ func Test_Register_User_100_times(t *testing.T) {
 			continue
 		}
 		start := time.Now()
+		memoryUsed += len(name)
 		err = cli.UserRegister()
 		stats.AddTime(time.Since(start))
 		if err != nil {
@@ -39,5 +40,7 @@ func Test_Register_User_100_times(t *testing.T) {
 	t.Log("User identity register benchmark \n")
 	t.Logf("succes rate: %f%% \n", float64(success)/float64(requestSamples100)*100)
 	t.Logf("fail rate: %f%% \n", float64(fails)/float64(requestSamples100)*100)
+	t.Logf("throughput(bytes/second): %f%% \n", float64(memoryUsed)/stats.Calc().Time.Cumulative.Seconds())
+	t.Logf("memory (bytes): %d%% \n", memoryUsed)
 	t.Log(stats.Calc())
 }
